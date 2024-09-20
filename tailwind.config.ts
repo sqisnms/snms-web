8b31efb49366488987671878c3d067706447039b
Chaekang Lee <leechis@sqisoft.com>
2024-09-20 15:22:07 +0900

    tailwindcss 설정

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,19 +1,31 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss';
 
+import plugin from 'tailwindcss/plugin';
 const config: Config = {
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    './ui/**/.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        blue: { 400: '#2589FE', 500: '#0070F3', 600: '#2F6FEB' },
+      },
+      keyframes: {
+        shimmer: {
+          '0%': { transform: 'translateX(-100%)' },
+          '100%': { transform: 'translateX(100%)' },
+        },
+      },
+      animation: { shimmer: 'shimmer 2s 1' },
+      backgroundImage: {
+        'gradient-shimmer':
+          'linear-gradient(to right, transparent, rgba(255, 255, 255, 0.6), transparent',
       },
     },
   },
-  plugins: [],
+  plugins: [require('@tailwindcss/forms')],
 };
 export default config;
