87bb905632374ff9cad62d16942720aca32adf4a
Chaekang Lee <leechis@sqisoft.com>
2024-09-24 16:34:27 +0900

    chore: format

--- app/page.tsx
+++ app/page.tsx
@@ -6,15 +6,15 @@ import { shimmer } from '@/ui/animations';
 
 export default function Page() {
   return (
-    <main className='flex flex-col min-h-screen p-4'>
-      <div className={`${shimmer} relative overflow-hidden `}>
+    <main className='flex min-h-screen flex-col p-4'>
+      <div className={`${shimmer} relative overflow-hidden`}>
         {/* <div className="relative overflow-hidden before:absolute before:inset-0 before:-translate-x-full before:animate-shimmer before:bg-gradient-shimmer before:from-transparent before:via-white/60 before:to-transparent"> */}
-        <div className='flex items-center h-20 p-2 bg-blue-500 rounded-lg'>
+        <div className='flex h-20 items-center rounded-lg bg-blue-500 p-2'>
           <SNMSLogo />
         </div>
       </div>
-      <div className='flex flex-col gap-4 mt-4 grow md:flex-row'>
-        <div className='flex flex-col justify-center gap-6 px-6 py-10 rounded-lg bg-gray-50 md:w-3/5 md:px-20'>
+      <div className='mt-4 flex grow flex-col gap-4 md:flex-row'>
+        <div className='flex flex-col justify-center gap-6 rounded-lg bg-gray-50 px-6 py-10 md:w-3/5 md:px-20'>
           <p className='text-xl text-gray-800 md:text-3xl md:leading-normal'>
             <strong>Smart NMS에 오신걸 환영합니다.</strong> <br />
             스마트한 네트워크 관리 시스템입니다.
@@ -22,19 +22,19 @@ export default function Page() {
           <div className='flex gap-2'>
             <Link
               href='/login'
-              className='flex items-center self-start gap-2 px-6 py-3 text-sm font-medium text-white transition-colors bg-blue-500 rounded-lg hover:bg-blue-400 md:text-base'
+              className='flex items-center gap-2 self-start rounded-lg bg-blue-500 px-6 py-3 text-sm font-medium text-white transition-colors hover:bg-blue-400 md:text-base'
             >
               로그인 <ArrowRightIcon className='w-5 md:w-2 lg:w-6' />
             </Link>
             <Link
               href='/signup'
-              className='flex items-center self-start gap-2 px-6 py-3 text-sm font-medium text-white transition-colors bg-blue-500 rounded-lg hover:bg-blue-400 md:text-base'
+              className='flex items-center gap-2 self-start rounded-lg bg-blue-500 px-6 py-3 text-sm font-medium text-white transition-colors hover:bg-blue-400 md:text-base'
             >
               회원가입 <UserPlusIcon className='w-5 md:w-2 lg:w-6' />
             </Link>
           </div>
         </div>
-        <div className='flex items-center justify-center p-6 md:w-4/5 md:px-70 md:py-12'>
+        <div className='md:px-70 flex items-center justify-center p-6 md:w-4/5 md:py-12'>
           <Image
             src='/main-desktop.png'
             width={1000}
