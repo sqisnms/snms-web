674cc485af233d9e4f285c9c268367314d6f0d87
이상희 <esanghee84@gmail.com>
2024-10-15 18:55:13 +0900

    fix: DATABASE_URL > POSTGRES_URL

--- lib/db.ts
+++ lib/db.ts
@@ -3,7 +3,7 @@ import { Pool } from "pg"
 
 // PostgreSQL 연결 설정
 const pool = new Pool({
-  connectionString: process.env.DATABASE_URL, // 환경 변수에서 가져온 연결 URL
+  connectionString: process.env.POSTGRES_URL, // 환경 변수에서 가져온 연결 URL
 })
 
 export default pool // Pool 객체를 내보내서 재사용 가능하게 함
