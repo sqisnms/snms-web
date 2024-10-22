534ab999da577bd6cc40d25b95d4fe51b1e33833
이상희 <esanghee84@gmail.com>
2024-10-22 17:25:50 +0900

    feat: mui설정 추가

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,26 +1,28 @@
 "use client"
 
-import {
-  ClickHouseQuerySample,
-  MariaDBQuerySample,
-  PostgresQuerySample,
-} from "@/components/sample/Sample"
+import { dashboardSelectedAtom } from "@/atom/dashboardAtom"
+import { TopTable } from "@/components/dashboard/TopTable"
+import { ClickHouseQuerySample, PostgresQuerySample } from "@/components/sample/Sample"
+import { useAtom } from "jotai"
 
 export default function Page() {
+  const [selected] = useAtom(dashboardSelectedAtom)
   return (
     <div>
       <h1>Grafana Dashboard</h1>
+      <TopTable />
       <iframe
         width="100%"
         height="600"
         title="서버자원 모니터링"
-        src="grafana/public-dashboards/e3c44e0241b045f79980576d0e2d9229?orgId=1"
+        src={`grafana/d/ae0ijnes4j7cwe/server-resource?orgId=1&kiosk${selected}`}
+        // &var-server_id=sqinms_m01&var-server_id=sqinms_m03
       />
       <div>
         <h2>데이터베이스 통계</h2>
         <PostgresQuerySample />
         <ClickHouseQuerySample />
-        <MariaDBQuerySample />
+        {/* <MariaDBQuerySample /> */}
       </div>
     </div>
   )
