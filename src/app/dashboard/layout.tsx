0618dcb4251ba98a1296db71e33b50e1cb7e3e75
최진주 <jj.choi@sqisoft.com>
2024-10-29 13:52:38 +0900

    feat: GNB 추가에 의한 레이아웃 수정

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -1,12 +1,17 @@
-import SideNav from "@/components/dashboard/sidenav"
+// import SideNav from "@/components/dashboard/sidenav"
+import GNB from "@/components/dashboard/gnb"
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
-    <div className="flex h-screen flex-col md:flex-row md:overflow-hidden">
-      <div className="w-full flex-none md:w-64">
+    <div className="flex h-screen flex-col">
+      {/* <div className="w-full flex-none md:w-64">
         <SideNav />
       </div>
-      <div className="flex-grow p-6 md:overflow-y-auto md:p-12">{children}</div>
+      <div className="flex-grow p-6 md:overflow-y-auto md:p-12">{children}</div> */}
+      <div className="w-full">
+        <GNB />
+      </div>
+      <div className="w-full p-12 p-6">{children}</div>
     </div>
   )
 }
