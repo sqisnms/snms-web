7fbd523059e1f6c9acc14894dda44f10c0ed6ae7
이상희 <esanghee84@gmail.com>
2024-11-07 16:00:05 +0900

    fix: 회원가입/로그인 화면 수정, 탭화면 샘플

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -1,15 +1,19 @@
 import SignUpForm from "@/components/account/signup-form"
-import SNMSLogo from "@/components/logo"
+import { Box } from "@mui/material"
+import Image from "next/image"
 
 export default function LoginPage() {
   return (
-    <main className="flex items-center justify-center md:h-screen">
-      <div className="relative mx-auto flex w-full max-w-[400px] flex-col space-y-2.5 p-4 md:-mt-32">
-        <div className="flex h-20 w-full items-end rounded-lg bg-blue-500 p-3 md:h-36">
-          <SNMSLogo />
-        </div>
-        <SignUpForm />
-      </div>
-    </main>
+    <Box className="h-screen bg-cover bg-center" sx={{ backgroundImage: "url('/login_bg.png')" }}>
+      <Box className="flex h-full w-full items-center justify-center p-8">
+        <Box className="w-full max-w-md rounded-lg bg-white bg-opacity-90 px-10 py-12 shadow-lg">
+          <Box className="mb-2 flex justify-center">
+            <Image src="/logo.png" width="130" height="40" alt="로고" className="h-10" />
+          </Box>
+          <h1 className="mb-8 text-center text-2xl font-semibold text-primary">회원가입</h1>
+          <SignUpForm />
+        </Box>
+      </Box>
+    </Box>
   )
 }
