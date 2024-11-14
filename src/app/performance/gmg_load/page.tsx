eac25756e02bd51868693a895e40b2aabab56dd1
이상희 <esanghee84@gmail.com>
2024-11-14 13:39:47 +0900

    feat: 라이트모드 및 그라파나 화면 추가

---
+++ src/app/performance/gmg_load/page.tsx
@@ -0,0 +1,21 @@
+"use client"
+
+import { grafanaServerProcessParamAtom } from "@/atom/dashboardAtom"
+import { GrafanaIframe } from "@/components/dashboard/GrafanaIframe"
+import { useAtom } from "jotai"
+
+export default function Page() {
+  const [selected] = useAtom(grafanaServerProcessParamAtom)
+  return (
+    <>
+      {/* <div style={{ width: 50, height: 20 }} /> */}
+      {/* <ProcessOption /> */}
+      {/* <div style={{ width: 50, height: 20 }} /> */}
+      <GrafanaIframe
+        src="/grafana/d/ee3p929dwdukge/gmg-type2?kiosk"
+        selected={selected}
+        title="gmg2"
+      />
+    </>
+  )
+}
