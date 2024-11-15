3cb6ec2052da9bcadcc0c35aa68636d7ed736f3c
최진주 <jj.choi@sqisoft.com>
2024-11-15 17:23:59 +0900

    feat: GNB dark mode 추가, 양옆 공간 간격 조정

--- src/components/common/Gnb.tsx
+++ src/components/common/Gnb.tsx
@@ -1,22 +1,21 @@
 import { LogoutButton } from "@/components/account/logout-form"
 import { MenuType } from "@/types/menu"
-import Image from "next/image"
 import Menu from "./Menu"
 
 function Gnb({ menuData, userName }: { menuData: MenuType[]; userName: string }) {
   return (
-    <header className="bg-primary text-white">
-      <div className="container mx-auto flex h-16 items-center justify-between">
+    <header className="bg-primary text-white dark:bg-gray-800">
+      <div className="container flex h-16 w-full max-w-full items-center justify-between px-4">
         <Menu menuData={menuData} />
 
         {/* Right Icons */}
         <div className="flex items-center space-x-4">
-          <button type="button">
+          {/* <button type="button">
             <Image src="/ic_setting.png" width={25} height={25} alt="Settings" className="h-6" />
           </button>
           <button type="button">
             <Image src="/ic_search.png" width={25} height={25} alt="Search" className="h-6" />
-          </button>
+          </button> */}
           <span>{userName}님</span>
           {/* <UserName /> */}
           <LogoutButton />
