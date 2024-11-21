2d53d20bda2a7393eb2a0d8de2fc1d455dca8323
이상희 <esanghee84@gmail.com>
2024-11-21 18:46:09 +0900

    fix: 다크모드 토글 시 실시간 변경 안함(새로고침으로 적용)

--- src/components/common/Menu.tsx
+++ src/components/common/Menu.tsx
@@ -187,10 +187,10 @@ export default function Menu({
   // 스위치가 눌렸을 때 모드 전환 함수
   const toggleTheme = (event: React.ChangeEvent<HTMLInputElement>) => {
     const newTheme = event.target.checked ? "dark" : "light"
-    setTheme(newTheme)
-    setGrafanaTheme(newTheme as "dark" | "light")
+    // setTheme(newTheme)
+    // setGrafanaTheme(newTheme as "dark" | "light")
 
-    document.documentElement.className = newTheme
+    // document.documentElement.className = newTheme
 
     setCookie("theme", newTheme, 30)
 
