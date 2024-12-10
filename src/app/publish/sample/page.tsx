4eb3a4d41a38542f43d601ef0870cd8ea0d78ba1
최진주 <jj.choi@sqisoft.com>
2024-12-10 15:57:05 +0900

    feat: 샘플 페이지 - 테이블 디자인 수정, dark 모드 추가

--- src/app/publish/sample/page.tsx
+++ src/app/publish/sample/page.tsx
@@ -30,7 +30,7 @@ const DashboardLayout: React.FC = () => {
   return (
     <Box className="p-0">
       {/* Header Section */}
-      <Paper className="mb-6 w-full p-0 shadow-md">
+      <Paper className="mb-6 w-full p-0 shadow-md dark:bg-black">
         {/* <Box display="flex" alignItems="center" justifyContent="space-between"> */}
         {/* Title */}
         {/* <Typography variant="h5" className="font-semibold text-gray-800">
@@ -47,16 +47,19 @@ const DashboardLayout: React.FC = () => {
         {/* </Box> */}
 
         <Box display="flex" alignItems="center" justifyContent="space-between" className="w-full">
-          <Typography variant="h6" className="text-lg font-semibold text-gray-800">
+          <Typography variant="h6" className="text-lg font-semibold text-gray-800 dark:text-white">
             Page Sub Title
           </Typography>
           {/* Select Box */}
 
           <Box display="flex" alignItems="center">
-            <Typography variant="body1" className="text-sm font-bold text-gray-700">
+            <Typography
+              variant="body1"
+              className="p-0 text-sm font-bold text-gray-700 dark:text-white"
+            >
               선택 항목
             </Typography>
-            <Select defaultValue="Option 1" className="ml-2 h-10 bg-white text-sm">
+            <Select defaultValue="Option 1" className="ml-2 h-10 bg-white text-sm dark:bg-black">
               <MenuItem value="Option 1" className="text-sm">
                 Option 1
               </MenuItem>
@@ -64,7 +67,7 @@ const DashboardLayout: React.FC = () => {
                 Option 2
               </MenuItem>
             </Select>
-            <Typography variant="body1" className="ml-6 text-sm text-gray-700">
+            <Typography variant="body1" className="ml-6 text-sm text-gray-700 dark:text-white">
               조회건수 <span className="font-bold"> 10건</span>
             </Typography>
           </Box>
@@ -76,54 +79,62 @@ const DashboardLayout: React.FC = () => {
             value={tabValue}
             onChange={handleTabChange}
             aria-label="styled tabs"
-            className="w-full border-b border-gray-300"
-            TabIndicatorProps={{
-              style: {
-                backgroundColor: "primary",
-                height: "2px",
-              },
-            }}
+            className="w-full border-b border-gray-300 dark:border-gray-800"
+            sx={[
+              (theme) => ({
+                "& .MuiTabs-indicator": {
+                  backgroundColor: "primary.main",
+                  height: "2px",
+                  ...theme.applyStyles("dark", {
+                    backgroundColor: "theme.palette.secondary.main",
+                  }),
+                },
+              }),
+            ]}
           >
             <Tab
               label="Tab 1"
               className={`max-w-none flex-1 px-4 py-2 font-semibold text-gray-700 ${
                 tabValue === 0
-                  ? "border-l border-r border-t border-gray-300 bg-white"
-                  : "bg-gray-100"
+                  ? "border-l border-r border-t border-gray-300 bg-white dark:bg-black dark:text-secondary"
+                  : "bg-gray-100 dark:bg-black dark:text-white"
               }`}
             />
             <Tab
               label="Tab 2"
               className={`max-w-none flex-1 px-4 py-2 font-semibold text-gray-700 ${
                 tabValue === 1
-                  ? "border-l border-r border-t border-gray-300 bg-white"
-                  : "bg-gray-100"
+                  ? "border-l border-r border-t border-gray-300 bg-white dark:bg-black dark:text-secondary"
+                  : "bg-gray-100 dark:bg-black dark:text-white"
               }`}
             />
             <Tab
               label="Tab 3"
               className={`max-w-none flex-1 px-4 py-2 font-semibold text-gray-700 ${
                 tabValue === 2
-                  ? "border-l border-r border-t border-gray-300 bg-white"
-                  : "bg-gray-100"
+                  ? "border-l border-r border-t border-gray-300 bg-white dark:bg-black dark:text-secondary"
+                  : "bg-gray-100 dark:bg-black dark:text-white"
               }`}
             />
             <Tab
               label="Tab 4"
               className={`max-w-none flex-1 px-4 py-2 font-semibold text-gray-700 ${
                 tabValue === 3
-                  ? "border-l border-r border-t border-gray-300 bg-white"
-                  : "bg-gray-100"
+                  ? "border-l border-r border-t border-gray-300 bg-white dark:bg-black dark:text-secondary"
+                  : "bg-gray-100 dark:bg-black dark:text-white"
               }`}
             />
           </Tabs>
         </Box>
 
-        <div className="mb-4 mt-4 rounded-sm border border-gray-300 bg-gray-50 p-4">
+        <div className="mb-4 mt-4 rounded-sm border border-gray-300 bg-gray-50 p-4 dark:border-gray-700 dark:bg-gray-950">
           <div className="mb-4 grid grid-cols-3 gap-x-4 gap-y-2">
             {/* 운용부서 */}
             <div>
-              <label htmlFor="select1" className="mb-1 block text-sm font-medium text-gray-700">
+              <label
+                htmlFor="select1"
+                className="mb-1 block text-sm font-medium text-gray-700 dark:text-white"
+              >
                 운용부서
               </label>
               <Select
@@ -131,7 +142,7 @@ const DashboardLayout: React.FC = () => {
                 variant="outlined"
                 size="small"
                 fullWidth
-                className="bg-white text-sm"
+                className="bg-white text-sm dark:bg-gray-400"
               >
                 <MenuItem value="" className="text-sm">
                   선택하세요
@@ -147,7 +158,10 @@ const DashboardLayout: React.FC = () => {
 
             {/* 운용상태 */}
             <div>
-              <label htmlFor="select2" className="mb-1 block text-sm font-medium text-gray-700">
+              <label
+                htmlFor="select2"
+                className="mb-1 block text-sm font-medium text-gray-700 dark:text-white"
+              >
                 운용상태
               </label>
               <Select
@@ -165,7 +179,10 @@ const DashboardLayout: React.FC = () => {
 
             {/* NMS공사/작업용 장비 유형 */}
             <div>
-              <label htmlFor="select3" className="mb-1 block text-sm font-medium text-gray-700">
+              <label
+                htmlFor="select3"
+                className="mb-1 block text-sm font-medium text-gray-700 dark:text-white"
+              >
                 NMS공사/작업용 장비 유형
               </label>
               <Select
@@ -183,7 +200,10 @@ const DashboardLayout: React.FC = () => {
 
             {/*  장비ID(영) */}
             <div>
-              <label htmlFor="input1" className="mb-1 block text-sm font-medium text-gray-700">
+              <label
+                htmlFor="input1"
+                className="mb-1 block text-sm font-medium text-gray-700 dark:text-white"
+              >
                 장비ID(영)
               </label>
               <TextField
@@ -196,7 +216,10 @@ const DashboardLayout: React.FC = () => {
 
             {/*  장비ID(한) */}
             <div>
-              <label htmlFor="input2" className="mb-1 block text-sm font-medium text-gray-700">
+              <label
+                htmlFor="input2"
+                className="mb-1 block text-sm font-medium text-gray-700 dark:text-white"
+              >
                 장비ID(한)
               </label>
               <TextField
@@ -209,7 +232,10 @@ const DashboardLayout: React.FC = () => {
 
             {/*  장비ID(한) */}
             <div>
-              <label htmlFor="input3" className="mb-1 block text-sm font-medium text-gray-700">
+              <label
+                htmlFor="input3"
+                className="mb-1 block text-sm font-medium text-gray-700 dark:text-white"
+              >
                 장비 IP
               </label>
               <TextField
@@ -222,7 +248,10 @@ const DashboardLayout: React.FC = () => {
 
             {/* 장비구분(Lv1) */}
             <div>
-              <label htmlFor="select4" className="mb-1 block text-sm font-medium text-gray-700">
+              <label
+                htmlFor="select4"
+                className="mb-1 block text-sm font-medium text-gray-700 dark:text-white"
+              >
                 장비구분(Lv1)
               </label>
               <Select
@@ -240,7 +269,10 @@ const DashboardLayout: React.FC = () => {
 
             {/* 장비구분(Lv2) */}
             <div>
-              <label htmlFor="select5" className="mb-1 block text-sm font-medium text-gray-700">
+              <label
+                htmlFor="select5"
+                className="mb-1 block text-sm font-medium text-gray-700 dark:text-white"
+              >
                 장비구분(Lv2)
               </label>
               <Select
@@ -258,7 +290,10 @@ const DashboardLayout: React.FC = () => {
 
             {/* 장비구분(Lv3) */}
             <div>
-              <label htmlFor="select6" className="mb-1 block text-sm font-medium text-gray-700">
+              <label
+                htmlFor="select6"
+                className="mb-1 block text-sm font-medium text-gray-700 dark:text-white"
+              >
                 장비구분(Lv3)
               </label>
               <Select
@@ -323,20 +358,47 @@ const DashboardLayout: React.FC = () => {
           </div>
         </div>
 
-        <TableContainer className="mt-5">
-          <Table aria-label="simple table">
-            <TableHead>
+        <TableContainer className="mt-5 rounded-md">
+          <Table
+            aria-label="simple table"
+            sx={[
+              (theme) => ({
+                "& .MuiTableCell-root": { padding: "4px 16px", height: "40px" },
+                backgroundColor: "#fafafa",
+                ...theme.applyStyles("dark", {
+                  backgroundColor: "#000",
+                }),
+              }),
+            ]}
+          >
+            <TableHead
+              sx={[
+                (theme) => ({
+                  height: "40px",
+                  background: "#e5e7eb",
+                  ...theme.applyStyles("dark", {
+                    backgroundColor: "#1f2937",
+                  }),
+                }),
+              ]}
+            >
               <TableRow>
-                <TableCell className="font-semibold text-gray-600">Column 1</TableCell>
-                <TableCell className="font-semibold text-gray-600">Column 2</TableCell>
-                <TableCell className="font-semibold text-gray-600">Column 3</TableCell>
+                <TableCell className="font-semibold text-gray-600 dark:text-white">
+                  Column 1
+                </TableCell>
+                <TableCell className="font-semibold text-gray-600 dark:text-white">
+                  Column 2
+                </TableCell>
+                <TableCell className="font-semibold text-gray-600 dark:text-white">
+                  Column 3
+                </TableCell>
               </TableRow>
             </TableHead>
             <TableBody>
               <TableRow>
-                <TableCell>Data 1</TableCell>
-                <TableCell>Data 2</TableCell>
-                <TableCell>Data 3</TableCell>
+                <TableCell className="dark:text-white">Data 1</TableCell>
+                <TableCell className="dark:text-white">Data 2</TableCell>
+                <TableCell className="dark:text-white">Data 3</TableCell>
               </TableRow>
               <TableRow>
                 <TableCell>Data 4</TableCell>
